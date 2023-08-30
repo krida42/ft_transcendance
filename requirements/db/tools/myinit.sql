7d1289198e890e17b9ca38ca69030be81ab56381
Kevin Isikayadir <iskrida42@gmail.com>
2023-08-30 20:11:30 +0200

    Now the nodejs server is connecting to the postgresql database that is in the container from the docker-compose

--- requirements/db/tools/myinit.sql
+++ requirements/db/tools/myinit.sql
@@ -1,16 +1,16 @@
-\connect transcendance
+\connect database_development
 
 -- Création de l'utilisateur avec les variables d'environnement
-CREATE USER $DB_USER WITH PASSWORD $DB_PASSWORD;
+-- CREATE USER $DB_USER WITH PASSWORD $DB_PASSWORD;
 
 -- Attribution des privilèges à la base de données avec les variables d'environnement
-GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO $DB_USER;
+-- GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO $DB_USER;
 
 -- Attribution des privilèges aux schémas avec les variables d'environnement
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL PRIVILEGES ON TABLES TO $DB_USER;
+-- ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL PRIVILEGES ON TABLES TO $DB_USER;
 
 
--- CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 
 -- CREATE TABLE users (
--- requirements/server/entrypoint.sh
+++ requirements/server/entrypoint.sh
@@ -2,6 +2,10 @@
 
 npm install
 
-npm run build
+# npm run build
+
+npm run db:migrate
+# npm run db:seed-dev
+
 exec npm run start:dev
 

82fc093359550d7894d0f5d0efea00e18f326a63
Vincent-Barbier-code <vincentpcrog@hotmail.com>
2023-09-14 20:38:12 +0200

    Revert "Revert "Ugly version without session and remastered auth""
    This reverts commit 5f19c40f2103f83260bc253257baa8ab342d93f3.

--- data/server/src/app/app.controller.ts
+++ data/server/src/app/app.controller.ts
@@ -1,12 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-
+import { Req, Res } from '@nestjs/common';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
+    // req.session.views = (req.session.views || 0) + 1
+    // Write response
+    // res.end(req.session.views + ' views')
+    
     return this.appService.getHello();
   }
 }

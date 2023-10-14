4cbb22049ef26e435f5c9ec2283cb8adec8c95fe
krida42 <94807307+krida42@users.noreply.github.com>
2023-10-14 20:28:07 +0200

    refactor css friends vue

--- data/client/src/views/FriendsView.vue
+++ data/client/src/views/FriendsView.vue
@@ -1,22 +1,18 @@
 <template>
   <div class="friends-view">
-    <div class="navigation-ctn bg-green-dark text-[1em]">
-      <div class="navigation bg-red-200s flex items-center flex-col">
-        <div class="pong-button bg-blue-600d self-start">
-          <MenuButton svg-name="pong-logo.svg" class="bg-red-500d w-[50x]" />
+    <div class="navigation-ctn bg-green-dark">
+      <div class="navigation bg-red-200s">
+        <div class="pong-button bg-blue-600d">
+          <MenuButton svg-name="pong-logo.svg" class="bg-red-500d" />
         </div>
-        <div
-          class="status-filters-ctn bg-red-800d w-full flex flex-col items-center"
-        >
-          <div class="filter-primary">
+        <div class="status-filters-ctn bg-red-800d">
+          <div class="filter-btn">
             <Icon size="3em" color="black" class="bg-red-200s">
               <Heart />
             </Icon>
             <p class="bg-blue-100d">Friends</p>
           </div>
-          <div
-            class="filters bd-redd flex flex-col items-start w-[45%] text-[1.2em]"
-          >
+          <div class="sub-filters bd-redd">
             <div class="filter-all">
               <!-- prettier-ignore -->
               <span class="circle bg-black"></span>
@@ -37,13 +33,13 @@
             </div>
           </div>
         </div>
-        <div class="requests-btn filter-primary">
+        <div class="requests-btn filter-btn">
           <Icon size="3em" color="black" class="bg-red-200s">
             <Heart />
           </Icon>
           <p class="bg-blue-100d">Requests</p>
         </div>
-        <div class="blocked-btn filter-primary">
+        <div class="blocked-btn filter-btn">
           <Icon size="3em" color="black" class="bg-red-200s">
             <Heart />
           </Icon>
@@ -52,7 +48,9 @@
       </div>
     </div>
     <div class="main bg-green-light">
-      <div class="friend-input"></div>
+      <div class="friend-input">
+        <input type="text" placeholder="Search for friends" />
+      </div>
       <div class="friends-list"></div>
     </div>
   </div>
@@ -70,54 +68,62 @@ import MenuButton from "@/components/MenuButton.vue";
   grid-template-columns: minmax(14em, 1fr) 7fr;
   height: 100vh;
   width: 100%;
-
-  //   box-sizing: border-box;
 }
 
-.filter-primary p {
-  color: black;
-  font-family: "Baumans", cursive;
-  padding-top: 0.5rem;
-  font-size: 1.6em;
-  line-height: normal;
-  padding-bottom: 0.5rem;
-}
-
-span.status-filter-item {
-  color: black;
-  font-family: "Baumans", cursive;
-  font-size: 1rem;
-}
-
-.circle {
-  width: 0.6rem;
-  height: 0.6rem;
-  border-radius: 50%;
-  border: 2px solid black;
-  display: inline-block;
-  margin-right: 0.5rem;
-}
-
-.filters {
-  font-family: "Baumans", cursive;
-  color: black;
-}
-
-.filter-primary {
+.navigation {
   display: flex;
+  flex-direction: column;
   align-items: center;
-  justify-content: start;
-  gap: 0.8rem;
-  //   background-color: white;
-  border-radius: 20px;
-  width: 90%;
-  padding-left: 1rem;
-  margin-block: 1rem;
-}
+  font-family: "Baumans", cursive;
+  color: black;
 
-.filter-primary:hover {
-  //   background-color: rgba($color: #ffffff, $alpha: 0.5);
-  background-color: rgba(#ffffff, 0.5);
-  cursor: pointer;
+  .pong-button {
+    align-self: flex-start;
+  }
+
+  .status-filters-ctn {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+  }
+  .filter-btn {
+    display: flex;
+    align-items: center;
+    justify-content: start;
+    gap: 0.8em;
+    border-radius: 20px;
+    width: 90%;
+    padding-left: 1rem;
+    margin-block: 1rem;
+
+    p {
+      padding-top: 0.5rem;
+      font-size: 1.6em;
+      line-height: normal;
+      padding-bottom: 0.5rem;
+    }
+  }
+
+  .filter-btn:hover {
+    //   background-color: rgba($color: #ffffff, $alpha: 0.5);
+    background-color: rgba(#ffffff, 0.5);
+    cursor: pointer;
+  }
+  .sub-filters {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    width: 45%;
+    font-size: 1.2em;
+    .circle {
+      width: 0.6rem;
+      height: 0.6rem;
+      border-radius: 50%;
+      border: 2px solid black;
+      display: inline-block;
+      margin-right: 0.5rem;
+    }
+  }
 }
 </style>

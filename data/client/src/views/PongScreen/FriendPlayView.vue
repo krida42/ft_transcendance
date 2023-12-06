56d13d4592d94f673137c7f5859363514143cf3d
Marine <marine.vue@me.com>
2023-12-06 17:46:32 +0100

    friend list search bar now resets pageId. Css improved to better fit the design

--- data/client/src/views/PongScreen/FriendPlayView.vue
+++ data/client/src/views/PongScreen/FriendPlayView.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="pong-screen flex flex-col justify-center gap-[0.5rem]">
     <p class="leading-tight">Choose a friend to invite</p>
-    <div class="friend-caroussel h-[50%] text-[1.6rem] flex justify-center">
+    <div class="friend-caroussel h-[50%] text-[1.5rem] flex justify-center">
       <img
         @click="decrementPageId"
         class="w-[3rem] rotate-180 cursor-pointer"
         src="@/assets/svg/right-arrow.svg"
       />
-      <div class="friend-list w-[75%] normal-case">
+      <div class="friend-list w-[80%] normal-case">
         <ul class="self-center">
           <li
-            class="py-[0.3rem] pr-[1rem]"
+            class="py-[0.3rem] pr-[1rem] text-left cursor-pointer"
             v-for="friend in friendsNestedArray[currentPageId]"
             :key="friend.id"
           >
@@ -22,7 +22,7 @@
         />
         <ul class="self-center pl-[1rem]">
           <li
-            class="py-[0.3rem]"
+            class="py-[0.3rem] text-left cursor-pointer"
             v-for="friend in friendsNestedArray[currentPageId + 1]"
             :key="friend.id"
           >
@@ -36,14 +36,21 @@
         src="@/assets/svg/right-arrow.svg"
       />
     </div>
-    <input
-      v-model="username"
-      placeholder="username..."
-      type="text"
-      minlength="3"
-      maxlength="15"
-      class="bg-black text-[1.5rem] border-2 border-[#828287] mx-[6rem] pl-[1rem]"
-    />
+    <div class="input-wrap relative">
+      <input
+        v-model="username"
+        placeholder="search a friend..."
+        type="text"
+        minlength="3"
+        maxlength="15"
+        class="bg-black text-[1.5rem] border-2 border-[#828287] mx-[8rem] pl-[1rem]"
+        @keydown="resetPageId"
+      />
+      <img
+        src="@/assets/svg/search-icon.svg"
+        class="w-[2.5rem] absolute top-0 right-[75px]"
+      />
+    </div>
   </div>
 </template>
 
@@ -81,6 +88,10 @@ function decrementPageId() {
     currentPageId.value--;
   }
 }
+
+function resetPageId() {
+  currentPageId.value = 0;
+}
 </script>
 
 <style lang="scss" scoped>
@@ -88,5 +99,27 @@ function decrementPageId() {
   display: grid;
   grid-template-columns: 1fr 2px 1fr;
   grid-template-rows: 1fr;
+}
+
+li::before {
+  content: "•";
+  color: white;
+  display: inline-block;
+  width: 1em;
+  margin-left: -1em;
+}
+
+li {
+  position: relative;
+  padding-left: 2rem;
+}
+
+li:hover:after {
+  content: "";
+  position: absolute;
+  left: 16%;
+  bottom: 0;
+  width: 70%;
+  border-bottom: 2px solid white;
 }
 </style>

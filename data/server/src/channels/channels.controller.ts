58adac955f12aaf17ddc4d9bcc18d9235aab0d1a
Sylvain Loquet <sloquet@student.42paris.fr>
2023-12-09 17:24:12 +0100

    switch channel controller req.user.public_id -> this.public_id

--- data/server/src/channels/channels.controller.ts
+++ data/server/src/channels/channels.controller.ts
@@ -28,34 +28,35 @@ import { ResponseUserDto } from 'src/users/dto/responseUser.dto';
 @ApiTags('channels v3 (jwt OFF)')
 @Controller('')
 export class ChannelsController {
-  // private public_id: string | null = null; // DEPR
-  // private static isFirstUserConnected = true; // DEPR
+  private public_id: string | null = null; // DEPR
+  private static isFirstUserConnected = true; // DEPR
 
   constructor(
     private readonly channelService: ChannelsService,
     private readonly channelGetService: ChannelsGetService,
     private readonly channelOpService: ChannelsOpService,
     private readonly utils: ChannelsUtilsService,
+    private readonly usersService: UsersService,
   ) {
-    // this.setcurrentId(); // DEPR
+    this.setcurrentId(); // DEPR
   }
 
-  // async setcurrentId() { // DEPR
-  //   // TEMP
-  //   let user: User | null;
-  //   if (ChannelsController.isFirstUserConnected === true) {
-  //     user = await this.usersService.findByLogin('marvin');
-  //     ChannelsController.isFirstUserConnected = false;
-  //   } else {
-  //     user = await this.usersService.findByLogin('ben');
-  //     ChannelsController.isFirstUserConnected = true;
-  //   }
-  //   if (user && user.public_id) {
-  //     req.user.public_id = user.public_id;
-  //   } else {
-  //     console.error("L'utilisateur ou sa propriété 'public_id' est null.");
-  //   }
-  // }
+  async setcurrentId() { // DEPR
+    // TEMP
+    let user: User | null;
+    if (ChannelsController.isFirstUserConnected === true) {
+      user = await this.usersService.findByLogin('marvin');
+      ChannelsController.isFirstUserConnected = false;
+    } else {
+      user = await this.usersService.findByLogin('ben');
+      ChannelsController.isFirstUserConnected = true;
+    }
+    if (user && user.public_id) {
+      this.public_id = user.public_id;
+    } else {
+      console.error("L'utilisateur ou sa propriété 'public_id' est null.");
+    }
+  }
 
   // @Post('/channels') OK
   // @Patch('/channels/:chanId') TOCHECK
@@ -103,20 +104,20 @@ export class ChannelsController {
   // @Get('/channels/:chanId/owner') OK
 
   @ApiOperation({ summary: 'Create a channel (dto)' })
-  @UseGuards(AuthGuard('jwt'), AuthGuard('jwt-2fa'))
+  // @UseGuards(AuthGuard('jwt'), AuthGuard('jwt-2fa'))
   @Post('/channels')
   async createChannel(
     @Req() req: ReqU,
     @Body() editChannelDto: EditChannelDto,
   ) {
     return this.channelService.createChannel(
-      req.user.public_id,
+      this.public_id,
       editChannelDto,
     );
   }
 
   @ApiOperation({ summary: 'Update a channel (dto)' })
-  @UseGuards(AuthGuard('jwt'), AuthGuard('jwt-2fa'))
+  // @UseGuards(AuthGuard('jwt'), AuthGuard('jwt-2fa'))
   @Patch('/channels/:chanId')
   async updateChannel(
     @Req() req: ReqU,
@@ -124,23 +125,23 @@ export class ChannelsController {
     @Body() editChannelDto: EditChannelDto,
   ) {
     return this.channelService.updateChannel(
-      req.user.public_id,
+      this.public_id,
       chanId,
       editChannelDto,
     );
   }
 
   @ApiOperation({ summary: 'Delete a channel' })
-  @UseGuards(AuthGuard('jwt'), AuthGuard('jwt-2fa'))
+  // @UseGuards(AuthGuard('jwt'), AuthGuard('jwt-2fa'))
   @Delete('/channels/:chanId')
   async deleteChannel(@Req() req: ReqU, @Param('chanId') chanId: uuidv4) {
-    return this.channelService.deleteChannel(req.user.public_id, chanId);
+    return this.channelService.deleteChannel(this.public_id, chanId);
   }
 
   // ---------- JOIN / QUIT
 
   @ApiOperation({ summary: 'Join a channel' })
-  @UseGuards(AuthGuard('jwt'), AuthGuard('jwt-2fa'))
+  // @UseGuards(AuthGuard('jwt'), AuthGuard('jwt-2fa'))
   @Post('/channels/:chanId/join')
   async joinChannel(
     @Req() req: ReqU,
@@ -148,17 +149,17 @@ export class ChannelsController {
     @Body() passwordChannelDto: PasswordChannelDto,
   ) {
     return this.channelService.joinChannel(
-      req.user.public_id,
+      this.public_id,
       chanId,
       passwordChannelDto,
     );
   }
 
   @ApiOperation({ summary: 'Quit a channel' })
-  @UseGuards(AuthGuard('jwt'), AuthGuard('jwt-2fa'))
+  // @UseGuards(AuthGuard('jwt'), AuthGuard('jwt-2fa'))
   @Delete('/channels/:chanId/quit')
   async quitChannel(@Req() req: ReqU, @Param('chanId') chanId: uuidv4) {
-    return this.channelService.quitChannel(req.user.public_id, chanId);
+    return this.channelService.quitChannel(this.public_id, chanId);
   }
 
   // ---------- ADMIN
@@ -170,94 +171,94 @@ export class ChannelsController {
     @Param('chanId') chanId: string,
     @Param('userId') userId: string,
   ) {
-    return this.channelOpService.addAdmin(req.user.public_id, chanId, userId);
+    return this.channelOpService.addAdmin(this.public_id, chanId, userId);
   }
 
   @ApiOperation({ summary: 'Delete admin userId' })
-  @UseGuards(AuthGuard('jwt'), AuthGuard('jwt-2fa'))
+  // @UseGuards(AuthGuard('jwt'), AuthGuard('jwt-2fa'))
   @Delete('/channels/:chanId/admin/:userId')
   async delAdmin(
     @Req() req: ReqU,
     @Param('chanId') chanId: string,
     @Param('userId') userId: string,
   ) {
-    return this.channelOpService.delAdmin(req.user.public_id, chanId, userId);
+    return this.channelOpService.delAdmin(this.public_id, chanId, userId);
   }
 
   // ---------- INVITE
   @ApiOperation({ summary: 'Invite userId to channel' })
-  @UseGuards(AuthGuard('jwt'), AuthGuard('jwt-2fa'))
+  // @UseGuards(AuthGuard('jwt'), AuthGuard('jwt-2fa'))
   @Post('/channels/:chanId/invite/:userId')
   async inviteToChannel(
     @Req() req: ReqU,
     @Param('chanId') chanId: string,
     @Param('userId') userId: string,
   ) {
-    return this.channelOpService.invite(req.user.public_id, chanId, userId);
+    return this.channelOpService.invite(this.public_id, chanId, userId);
   }
 
   @ApiOperation({ summary: 'Cancel userId invitation to channel' })
-  @UseGuards(AuthGuard('jwt'), AuthGuard('jwt-2fa'))
+  // @UseGuards(AuthGuard('jwt'), AuthGuard('jwt-2fa'))
   @Delete('/channels/:chanId/invite/:userId')
   async uninviteToChannel(
     @Req() req: ReqU,
     @Param('chanId') chanId: string,
     @Param('userId') userId: string,
   ) {
-    return this.channelOpService.uninvite(req.user.public_id, chanId, userId);
+    return this.channelOpService.uninvite(this.public_id, chanId, userId);
   }
 
   // ---------- BAN
   @ApiOperation({ summary: 'Ban userId from channel' })
-  @UseGuards(AuthGuard('jwt'), AuthGuard('jwt-2fa'))
+  // @UseGuards(AuthGuard('jwt'), AuthGuard('jwt-2fa'))
   @Post('/channels/:chanId/ban/:userId')
   async banUser(
     @Req() req: ReqU,
     @Param('chanId') chanId: string,
     @Param('userId') userId: string,
   ) {
-    return this.channelOpService.banUser(req.user.public_id, chanId, userId);
+    return this.channelOpService.banUser(this.public_id, chanId, userId);
   }
 
   @ApiOperation({ summary: 'Unban userId from channel' })
-  @UseGuards(AuthGuard('jwt'), AuthGuard('jwt-2fa'))
+  // @UseGuards(AuthGuard('jwt'), AuthGuard('jwt-2fa'))
   @Delete('/channels/:chanId/ban/:userId')
   async unbanUser(
     @Req() req: ReqU,
     @Param('chanId') chanId: string,
     @Param('userId') userId: string,
   ) {
-    return this.channelOpService.unbanUser(req.user.public_id, chanId, userId);
+    return this.channelOpService.unbanUser(this.public_id, chanId, userId);
   }
 
   // ---------- MUTE
   @ApiOperation({ summary: 'Mute userId in channel' })
-  @UseGuards(AuthGuard('jwt'), AuthGuard('jwt-2fa'))
+  // @UseGuards(AuthGuard('jwt'), AuthGuard('jwt-2fa'))
   @Patch('/channels/:chanId/mute/:userId')
   async muteUser(
     @Req() req: ReqU,
     @Param('chanId') chanId: string,
     @Param('userId') userId: string,
   ) {
-    return this.channelOpService.mute(req.user.public_id, chanId, userId);
+    return this.channelOpService.mute(this.public_id, chanId, userId);
   }
 
   // ---------- KICK
   @ApiOperation({ summary: 'Kick userId from channel' })
-  @UseGuards(AuthGuard('jwt'), AuthGuard('jwt-2fa'))
+  // @UseGuards(AuthGuard('jwt'), AuthGuard('jwt-2fa'))
   @Delete('/channels/:chanId/kick/:userId')
   async kickUser(
     @Req() req: ReqU,
     @Param('chanId') chanId: string,
     @Param('userId') userId: string,
   ) {
-    return this.channelOpService.kick(req.user.public_id, chanId, userId);
+    return this.channelOpService.kick(this.public_id, chanId, userId);
   }
 
   // ---------- GET CHANNEL DATA
 
   @ApiOperation({ summary: 'Get data of channel' })
-  @UseGuards(AuthGuard('jwt'), AuthGuard('jwt-2fa'))
+  // @UseGuards(AuthGuard('jwt'), AuthGuard('jwt-2fa'))
   @Get('/channels/:chanId')
   async getDataChan(@Req() req: ReqU, @Param('chanId') chanId: uuidv4) {
     return this.channelGetService.getDataChan(chanId);
@@ -268,45 +269,45 @@ export class ChannelsController {
   @ApiOperation({
     summary: 'Get joined channels (execpt direct chan) (sorted)',
   })
-  @UseGuards(AuthGuard('jwt'), AuthGuard('jwt-2fa'))
+  // @UseGuards(AuthGuard('jwt'), AuthGuard('jwt-2fa'))
   @Get('/channels-joined')
   async getJoinedChan(@Req() req: ReqU) {
-    return this.channelGetService.getJoinedChan(req.user.public_id);
+    return this.channelGetService.getJoinedChan(this.public_id);
   }
 
   @ApiOperation({ summary: 'Get direct joined channels (sorted)' })
-  @UseGuards(AuthGuard('jwt'), AuthGuard('jwt-2fa'))
+  // @UseGuards(AuthGuard('jwt'), AuthGuard('jwt-2fa'))
   @Get('/channels-direct')
   async getDirectChan(@Req() req: ReqU) {
-    return this.channelGetService.getDirectChan(req.user.public_id);
+    return this.channelGetService.getDirectChan(this.public_id);
   }
 
   @ApiOperation({ summary: 'Get available channels (sorted)' })
-  @UseGuards(AuthGuard('jwt'), AuthGuard('jwt-2fa'))
+  // @UseGuards(AuthGuard('jwt'), AuthGuard('jwt-2fa'))
   @Get('/channels-available')
   async getAvailableChan(@Req() req: ReqU) {
     return this.channelGetService.getUnjoinedChan(
-      req.user.public_id,
+      this.public_id,
       ChanType.Public,
     );
   }
 
   @ApiOperation({ summary: 'Get protected unjoined channels (sorted)' })
-  @UseGuards(AuthGuard('jwt'), AuthGuard('jwt-2fa'))
+  // @UseGuards(AuthGuard('jwt'), AuthGuard('jwt-2fa'))
   @Get('/channels-unjoined-protect')
   async getProtectedChan(@Req() req: ReqU) {
     return this.channelGetService.getUnjoinedChan(
-      req.user.public_id,
+      this.public_id,
       ChanType.Protected,
     );
   }
 
   @ApiOperation({ summary: 'Get private unjoined channels (sorted)' })
-  @UseGuards(AuthGuard('jwt'), AuthGuard('jwt-2fa'))
+  // @UseGuards(AuthGuard('jwt'), AuthGuard('jwt-2fa'))
   @Get('/channels-unjoined-private')
   async getPrivateChan(@Req() req: ReqU) {
     return this.channelGetService.getUnjoinedChan(
-      req.user.public_id,
+      this.public_id,
       ChanType.Private,
     );
   }
@@ -316,49 +317,49 @@ export class ChannelsController {
   @ApiOperation({
     summary: 'Get all users list include mutes,admins,owner)',
   })
-  @UseGuards(AuthGuard('jwt'), AuthGuard('jwt-2fa'))
+  // @UseGuards(AuthGuard('jwt'), AuthGuard('jwt-2fa'))
   @Get('/channels/:chanId/users')
   async getUsersChan(@Req() req: ReqU, @Param('chanId') chanId: uuidv4) {
     return this.channelGetService.getUsersChan(chanId);
   }
 
   @ApiOperation({ summary: 'Get only users/muted list' })
-  @UseGuards(AuthGuard('jwt'), AuthGuard('jwt-2fa'))
+  // @UseGuards(AuthGuard('jwt'), AuthGuard('jwt-2fa'))
   @Get('/channels/:chanId/users-only')
   async getUsersOnlyChan(@Req() req: ReqU, @Param('chanId') chanId: uuidv4) {
     return this.channelGetService.getUsersOnlyChan(chanId);
   }
 
   @ApiOperation({ summary: 'Get mutes list' })
-  @UseGuards(AuthGuard('jwt'), AuthGuard('jwt-2fa'))
+  // @UseGuards(AuthGuard('jwt'), AuthGuard('jwt-2fa'))
   @Get('/channels/:chanId/mutes')
   async getMutesChan(@Req() req: ReqU, @Param('chanId') chanId: uuidv4) {
     return this.channelGetService.getMutesChan(chanId);
   }
 
   @ApiOperation({ summary: 'Get admins (owner include) list' })
-  @UseGuards(AuthGuard('jwt'), AuthGuard('jwt-2fa'))
+  // @UseGuards(AuthGuard('jwt'), AuthGuard('jwt-2fa'))
   @Get('/channels/:chanId/admins')
   async getAdminsChan(@Req() req: ReqU, @Param('chanId') chanId: uuidv4) {
     return this.channelGetService.getAdminsChan(chanId);
   }
 
   @ApiOperation({ summary: 'Get invites list' })
-  @UseGuards(AuthGuard('jwt'), AuthGuard('jwt-2fa'))
+  // @UseGuards(AuthGuard('jwt'), AuthGuard('jwt-2fa'))
   @Get('/channels/:chanId/invites')
   async getInvitesChan(@Req() req: ReqU, @Param('chanId') chanId: uuidv4) {
     return this.channelGetService.getInvitesChan(chanId);
   }
 
   @ApiOperation({ summary: 'Get bans list' })
-  @UseGuards(AuthGuard('jwt'), AuthGuard('jwt-2fa'))
+  // @UseGuards(AuthGuard('jwt'), AuthGuard('jwt-2fa'))
   @Get('/channels/:chanId/bans')
   async getBansChan(@Req() req: ReqU, @Param('chanId') chanId: uuidv4) {
     return this.channelGetService.getBansChan(chanId);
   }
 
   @ApiOperation({ summary: 'Get owner' })
-  @UseGuards(AuthGuard('jwt'), AuthGuard('jwt-2fa'))
+  // @UseGuards(AuthGuard('jwt'), AuthGuard('jwt-2fa'))
   @Get('/channels/:chanId/owner')
   async getOwnerChan(@Req() req: ReqU, @Param('chanId') chanId: uuidv4) {
     const userStatuses = [UserStatus.Owner];

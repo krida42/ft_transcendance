a1b5e3ffbbda87789ac3189cf0d5b08f1da5e718
Sylvain Loquet <sloquet@student.42paris.fr>
2023-12-10 23:50:00 +0100

    add no-misused-promises & no-floating-promises

--- data/server/.eslintrc.js
+++ data/server/.eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'no-floating-promise'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
@@ -21,5 +21,15 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-floating-promises': 'error',
+    '@typescript-eslint/no-misused-promises': [
+      'error',
+      {
+        checksConditionals: true,
+        checksSpreads: true,
+        checksVoidReturn: false,
+      },
+    ],
+    "no-floating-promise/no-floating-promise": 2,
   },
 };

b24fd8242a3702a6c5a01984b9c1bd643c7b1fbe
Sylvain Loquet <sloquet@student.42paris.fr>
2024-01-04 18:10:18 +0100

    add typescript flags & remove seeders execpt achievements seeds

--- data/client/.eslintrc.js
+++ data/client/.eslintrc.js
@@ -13,8 +13,14 @@ module.exports = {
     ecmaVersion: 2020,
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
   },
   overrides: [
     {
